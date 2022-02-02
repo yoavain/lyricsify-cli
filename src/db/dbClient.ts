57b8dc1ae6c18052d0dee05f44466513c3da504b
Yoav Vainrich <yoavain@gmail.com>
2022-02-02 21:04:04 +0200

    * Lazy init dbClient

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -1,9 +1,7 @@
-import SQLiteTagSpawned from "sqlite-tag-spawned";
 import * as path from "path";
 
 const DB_FILE_PATH = path.join(__dirname, "..", "dev.sqlite3");
 const BIN_RELATIVE_PATH = "bin/sqlite/sqlite3.exe";
-const { get, query } = SQLiteTagSpawned(DB_FILE_PATH, { bin: BIN_RELATIVE_PATH });
 
 const schema = "main";
 const lyricsTable = "lyrics";
@@ -16,9 +14,18 @@ type LyricsRow = {
     lyrics: string
 }
 
+let dbClient;
+const getDbClient = () => {
+    if (!dbClient) {
+        const SQLiteTagSpawned = require("sqlite-tag-spawned");
+        dbClient = new SQLiteTagSpawned(DB_FILE_PATH, BIN_RELATIVE_PATH);
+    }
+    return dbClient;
+};
+
 export const getLyricsFromDb = async (artist: string, title: string): Promise<string> => {
     try {
-        const row: LyricsRow = await get`SELECT * from ${schema}.${lyricsTable} WHERE artist=${artist} AND title=${title}`;
+        const row: LyricsRow = await getDbClient().get`SELECT * from ${schema}.${lyricsTable} WHERE artist=${artist} AND title=${title}`;
         return row?.lyrics;
     }
     catch (e) {
@@ -27,9 +34,9 @@ export const getLyricsFromDb = async (artist: string, title: string): Promise<st
 };
 
 export const putLyricsInDb = async (artist: string, title: string, language: string, lyrics: string): Promise<void> => {
-    return query`INSERT OR REPLACE INTO ${schema}.${lyricsTable} (artist,title,language,lyrics) VALUES (${artist},${title},${language},${lyrics})`;
+    return getDbClient().query`INSERT OR REPLACE INTO ${schema}.${lyricsTable} (artist,title,language,lyrics) VALUES (${artist},${title},${language},${lyrics})`;
 };
 
 export const deleteLyricsFromDb = async (artist: string, title: string): Promise<void> => {
-    return query`${`DELETE FROM ${lyricsTable} WHERE artist=${artist} AND title=${title}`}`;
+    return getDbClient().query`${`DELETE FROM ${lyricsTable} WHERE artist=${artist} AND title=${title}`}`;
 };
