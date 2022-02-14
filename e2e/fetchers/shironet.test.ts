738742099c513496303af03c007ca203f20b0a0a
Yoav Vainrich <yoavain@gmail.com>
2022-02-14 18:27:11 +0200

    Puppeteer working in runtime, failing in jest (inside function)

--- e2e/fetchers/shironet.test.ts
+++ e2e/fetchers/shironet.test.ts
@@ -1,10 +1,12 @@
-import { Shironet } from "~src/services/shironet";
+import { chunkToPairs, getSongSearchUrl, Shironet } from "~src/services/shironet";
 import type { Lyrics } from "~src/lyrics";
+import type { Browser, ElementHandle } from "puppeteer";
+import puppeteer from "puppeteer";
+import { ERROR_LYRICS_NOT_FOUND } from "~src/errors";
+
+jest.setTimeout(60000);
 
 describe("Test Shironet", () => {
-    beforeEach(() => {
-        jest.setTimeout(15000);
-    });
     afterEach(() => {
         jest.restoreAllMocks();
     });
@@ -12,5 +14,63 @@ describe("Test Shironet", () => {
     it("should return a valid Shironet lyrics", async () => {
         const lyrics: Lyrics = await Shironet.getLyrics("משינה", "שלג צח");
         expect(lyrics.lyrics.startsWith("הוא שוב יוצא אל המרפסת")).toBeTruthy();
+    });
+
+    it("Test puppeteer code", async () => {
+        const artist = "משינה";
+        const title = "שלג צח";
+
+        let browser: Browser;
+        try {
+            // Open search page
+            browser = await puppeteer.launch({ headless: true });
+            const page = await browser.newPage();
+            await page.goto(getSongSearchUrl(artist, title));
+            await page.waitForSelector(".search_results");
+
+            // Find all search_link_name_big elements
+            const searchElements: Array<ElementHandle> = await page.mainFrame().$$(".search_link_name_big");
+            const titleArtistPairs: Array<[ElementHandle, ElementHandle]> = chunkToPairs<ElementHandle>(searchElements);
+
+            let foundElement: ElementHandle;
+            for (const [titleElement, artistElement] of titleArtistPairs) {
+                const titleFromElement: string = await page.evaluate((el) => el?.textContent?.trim(), titleElement);
+                const artistFromElement: string = await page.evaluate((el) => el?.textContent?.trim(), artistElement);
+
+                // Exact match
+                if (artistFromElement === artist && titleFromElement === title) {
+                    foundElement = titleElement;
+                    break;
+                }
+            }
+
+            if (!foundElement) {
+                throw new Error(ERROR_LYRICS_NOT_FOUND);
+            }
+
+            await foundElement.click();
+            await page.waitForSelector(".artist_lyrics_text");
+
+            const lyricsElements: Array<ElementHandle> = await page.mainFrame().$$(".artist_lyrics_text");
+            if (lyricsElements.length === 0) {
+                throw new Error(ERROR_LYRICS_NOT_FOUND);
+            }
+
+            const lyrics: string = await page.evaluate((el) => el?.textContent?.trim(), lyricsElements[0]);
+            expect(lyrics.startsWith("הוא שוב יוצא אל המרפסת")).toBeTruthy();
+
+            return {
+                language: "heb",
+                lyrics: lyrics
+            };
+
+        }
+        catch (e) {
+            console.error(e);
+            throw new Error(ERROR_LYRICS_NOT_FOUND);
+        }
+        finally {
+            await browser?.close();
+        }
     });
 });
--- src/services/shironet.ts
+++ src/services/shironet.ts
@@ -1,67 +1,73 @@
-import type { OptionsOfTextResponseBody } from "got";
-import got from "got";
-import type { LyricsService } from "~src/services";
+import type { LyricsService } from "~src/services/interface";
 import type { Lyrics } from "~src/lyrics";
 import { ERROR_LYRICS_NOT_FOUND } from "~src/errors";
+import type { Browser, ElementHandle } from "puppeteer";
+import puppeteer from "puppeteer";
 
 const SHIRONET_BASE_URL = "https://shironet.mako.co.il";
 
-const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.11 Safari/537.36";
-
 const LANGUAGE = "heb";
 
-const LYRICS_REGEXP = /<span\s+itemprop="Lyrics"\s+class="artist_lyrics_text">(?<lyrics>[^\\/]+)/gm;
-
-const getSongSearchUrl = (artist: string, title: string): string => {
-    return `${SHIRONET_BASE_URL}/searchSongs?q=${encodeURIComponent(`${artist} ${title}`)}&type=lyrics`;
+export const getSongSearchUrl = (artist: string, title: string): string => {
+    return `${SHIRONET_BASE_URL}/searchSongs?q=${encodeURIComponent(`"${artist}" "${title}"`)}&type=lyrics`;
 };
 
-const wrap = (str: string): string => {
-    return str.split(" ").map((part: string) => `<b>${part}</b>`).join(" ");
-};
-
-const getSongRegex = (artist: string, title: string): RegExp => {
-    return new RegExp(
-        "<a\\shref=\"(?<songurl>\\/artist\\?type=lyrics&lang=1&prfid=\\d+&wrkid=\\d+)\"\\sclass=\"search_link_name_big\">\\s+" +
-        `${wrap(title)}<\\/a>\\s+-\\s+` + 
-        "<a href=\"(?<artisturl>\\/artist\\?lang=1&prfid=\\d+)\"\\s+class=\"search_link_name_big\">\\s+" +
-        `${wrap(artist)}<\\/a>`,
-        "gm");
-};
-
-const cleanLyrics = (lyrics: string): string => {
-    return lyrics.replace(/<br>/g, "\n").replace(/</g, "");
-};
+export const chunkToPairs = <T>(arr: T[]): Array<[T, T]> => [...Array(Math.ceil(arr.length / 2))].map<[T, T]>((_, i) => [arr[2 * i], arr[2 * i + 1]]);
 
 export const Shironet: LyricsService = {
     async getLyrics(artist: string, title: string): Promise<Lyrics> {
-        // Search
-        const songSearchUrl: string = getSongSearchUrl(artist, title);
-        const options: OptionsOfTextResponseBody = {
-            decompress: true,
-            headers: {
-                "user-agent": USER_AGENT,
-                "cache-control": "max-age=0"
+        let browser: Browser;
+
+        try {
+            // Open search page
+            browser = await puppeteer.launch({ headless: false });
+            const page = await browser.newPage();
+            await page.goto(getSongSearchUrl(artist, title));
+            await page.waitForSelector(".search_results");
+
+            // Find all search_link_name_big elements
+            // const searchElements: Array<ElementHandle> = await page.mainFrame().$$(".search_link_name_big");
+            const searchElements: Array<ElementHandle> = await page.$$(".search_link_name_big");
+            const titleArtistPairs: Array<[ElementHandle, ElementHandle]> = chunkToPairs<ElementHandle>(searchElements);
+
+            let foundElement: ElementHandle;
+            for (const [titleElement, artistElement] of titleArtistPairs) {
+                const titleFromElement: string = await page.evaluate((el) => el?.textContent?.trim(), titleElement);
+                const artistFromElement: string = await page.evaluate((el) => el?.textContent?.trim(), artistElement);
+
+                // Exact match
+                if (artistFromElement === artist && titleFromElement === title) {
+                    foundElement = titleElement;
+                    break;
+                }
             }
-        };
-        const songSearchResultHtml = await got.get(songSearchUrl, options).then((res) => res.body);
-        const songRegex: RegExp = getSongRegex(artist, title);
-        const songMatch = songRegex.exec(songSearchResultHtml);
-        if (!songMatch || !songMatch.groups || !songMatch.groups.songurl) {
+
+            if (!foundElement) {
+                throw new Error(ERROR_LYRICS_NOT_FOUND);
+            }
+
+            await foundElement.click();
+            await page.waitForSelector(".artist_lyrics_text");
+
+            const lyricsElements: Array<ElementHandle> = await page.mainFrame().$$(".artist_lyrics_text");
+            if (lyricsElements.length === 0) {
+                throw new Error(ERROR_LYRICS_NOT_FOUND);
+            }
+
+            const lyrics: string = await page.evaluate((el) => el?.textContent?.trim(), lyricsElements[0]);
+
+            return {
+                language: LANGUAGE,
+                lyrics: lyrics
+            };
+
+        }
+        catch (e) {
+            console.error(e);
             throw new Error(ERROR_LYRICS_NOT_FOUND);
         }
-
-        // Fetch
-        const songUrl = `${SHIRONET_BASE_URL}${songMatch.groups.songurl}`;
-        const songResultHtml = await got.get(songUrl, options).then((res) => res.body);
-        const lyrics = LYRICS_REGEXP.exec(songResultHtml);
-        if (!lyrics || !lyrics.groups || !lyrics.groups.lyrics) {
-            throw new Error(ERROR_LYRICS_NOT_FOUND);
+        finally {
+            await browser?.close();
         }
-
-        return {
-            language: LANGUAGE,
-            lyrics: cleanLyrics(lyrics.groups.lyrics)
-        };
     }
 };
