762df388649f25a0684ec82e2e566a757b4e8193
Yoav Vainrich <yoavain@gmail.com>
2022-02-17 21:22:36 +0200

    Test puppeteer

---
+++ e2e/puppeteer.test.ts
@@ -0,0 +1,37 @@
+import { getBrowser } from "~src/puppeteerUtils";
+import type { Browser, Page } from "puppeteer";
+
+jest.setTimeout(60000);
+
+describe("Test Puppeteer on GitHub actions", () => {
+    let browser: Browser;
+    beforeAll(async () => {
+        browser = await getBrowser();
+    });
+    afterAll(async () => {
+        jest.restoreAllMocks();
+        await browser.close();
+    });
+
+    it("should get page", async () => {
+        let page: Page;
+        try {
+            // Open search page
+            const browser: Browser = await getBrowser();
+            page = await browser.newPage();
+            await Promise.all([
+                page.goto("https://www.google.com/"),
+                page.waitForNavigation()
+            ]);
+        }
+        catch (e) {
+            console.error(e);
+            throw e;
+        }
+        finally {
+            if (page) {
+                await page.close();
+            }
+        }
+    });
+});
