48436f4368eefdb3a6f53dff19d9231a184d3100
Yoav Vainrich <yoavain@gmail.com>
2022-03-08 21:21:13 +0200

    Log e2e test

--- e2e/puppeteerUtils.test.ts
+++ e2e/puppeteerUtils.test.ts
@@ -16,23 +16,30 @@ describe("Test PuppeteerUtils", () => {
     it("Should use all utils as expected", async () => {
         let page: Page;
         try {
+            console.log("pageLoad");
             page = await pageLoad("https://www.wikipedia.org/", ".central-textlogo-wrapper");
 
+            console.log("getPageUrl");
             const pageUrl: string = getPageUrl(page);
             expect(pageUrl).toBe("https://www.wikipedia.org/");
 
+            console.log("findElements");
             const elements: Array<ElementHandle> = await findElements(page, ".link-box");
             expect(elements.length).toBeGreaterThan(0);
 
+            console.log("clickElement");
             await clickElement(page, elements[0], ".mw-body");
 
+            console.log("findElements");
             const headlineElements: Array<ElementHandle> = await findElements(page, ".mw-headline");
             const headlineIds: string[] = await Promise.all(headlineElements.map((element) => {
+                console.log("getElementText");
                 return getElementText(element, "id");
             }));
             expect(headlineIds.filter(Boolean).length).toBeTruthy();
         }
         finally {
+            console.log("closePage");
             await closePage(page);
         }
     });
