cbbfa530dad04dfe507483abcc7de2041a763ed5
Yoav Vainrich <yoavain@gmail.com>
2022-05-14 11:15:41 +0300

    deps

--- src/config/cliConfig.ts
+++ src/config/cliConfig.ts
@@ -59,5 +59,5 @@ export const getCliConfig = (argv: string[]): CliConfig => {
         .version("version", version)
         .help()
         .strict()
-        .argv;
+        .argv as CliConfig;
 };
